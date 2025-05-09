852a3045af8ac0f8d44a694e8859ae296060c38c
Carlos P Mangueira Junior <cpeixejr@gmail.com>
2025-05-09 19:34:13 -0300

    feat: new profile model

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_09_222058) do
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
